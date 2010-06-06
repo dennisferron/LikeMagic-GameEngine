258825b64d2557a8669a92487fdb0e1e7a028614
unknown <dennis@.(none)>
2010-06-06 02:48:57 -0500

    Work in progress on TypeConvGraph

--- Include/LikeMagic/TypeConv/TypeConvGraph.hpp
+++ Include/LikeMagic/TypeConv/TypeConvGraph.hpp
@@ -8,28 +8,45 @@
 
 using namespace boost;
 
-#include "AbstractTypeConverter.hpp"
+#include "LikeMagic/TypeConv/AbstractTypeConverter.hpp"
+#include "LikeMagic/SFMO/AbstractExpression.hpp"
 
 namespace LikeMagic { namespace TypeConv {
+
+using LikeMagic::Utility::BetterTypeInfo;
+using namespace boost::graph;
+
+struct ConvChainBuilder;
 
 class TypeConvGraph
 {
 private:
-    std::map<BetterTypeInfo, int> indexes;
+    // To give it acess to our private typedefs.
+    friend struct ConvChainBuilder;
+
+    typedef AbstractTypeConverter const* p_conv_t;
+    typedef std::vector<p_conv_t> conv_vect_t;
 
     struct edge_info
     {
-        AbstractTypeConverter const* conv;
+        p_conv_t conv;
     };
 
     typedef adjacency_list<vecS, vecS, undirectedS, no_property, edge_info> graph_t;
-    graph_t graph;
+    typedef graph_traits<graph_t>::vertex_descriptor vertex_t;
+    typedef std::map<BetterTypeInfo, vertex_t> vertex_map_t;
 
+    graph_t graph;
+    vertex_map_t vertex_map;
+
+    bool has_type(BetterTypeInfo type) const;
     void add_type(BetterTypeInfo type);
+
+    static ExprPtr use_conv_chain(ExprPtr from, conv_vect_t const& chain, size_t pos=0);
 
 public:
     void add_conv(BetterTypeInfo from, BetterTypeInfo to, AbstractTypeConverter const* conv);
-    ExprPtr wrap_expr(ExprPtr from, BetterTypeInfo from, BetterTypeInfo to) const;
+    ExprPtr wrap_expr(ExprPtr from_expr, BetterTypeInfo from, BetterTypeInfo to) const;
 };
 
 
--- Source/TypeConvGraph.cpp
+++ Source/TypeConvGraph.cpp
@@ -1,17 +1,78 @@
 #include "LikeMagic/TypeConv/TypeConvGraph.hpp"
 
+#include "boost/graph/breadth_first_search.hpp"
+
+using namespace LikeMagic::SFMO;
+
 namespace LikeMagic { namespace TypeConv {
+
+bool TypeConvGraph::has_type(BetterTypeInfo type) const
+{
+    return vertex_map.find(type) != vertex_map.end();
+}
+
+void TypeConvGraph::add_type(BetterTypeInfo type)
+{
+    if (!has_type(type))
+        vertex_map[type] = add_vertex(graph);
+}
 
 void TypeConvGraph::add_conv(BetterTypeInfo from, BetterTypeInfo to, AbstractTypeConverter const* conv)
 {
     add_type(from);
     add_type(to);
-    add_edge(indexes[from], indexes[to], graph).second.conv = conv;
+    // TODO:  figure out correct syntax for accessing the edge property.
+    //add_edge(vertex_map[from], vertex_map[to], graph).second.conv = conv;
 }
 
-
-ExprPtr TypeConvGraph::wrap_expr(ExprPtr from, BetterTypeInfo from, BetterTypeInfo to) const
+struct ConvChainBuilder : public boost::default_bfs_visitor
 {
+    TypeConvGraph::vertex_t dest;
+    std::vector<AbstractTypeConverter const*> chain;
+
+    ConvChainBuilder(TypeConvGraph::vertex_t dest_)
+        : dest(dest_) {}
+
+    template <class Vertex, class Graph>
+    void discover_vertex(Vertex u, Graph& g)
+    {
+        if (u == dest)
+            throw chain;
+    }
+
+    template <typename Edge, typename Graph>
+    void tree_edge(Edge e, const Graph& g) const
+    {
+        // TODO: Figure out how to access the type conv object from the edge.
+        //chain.push_back(e.conv);
+    }
+};
+
+ExprPtr TypeConvGraph::use_conv_chain(ExprPtr from, conv_vect_t const& chain, size_t pos)
+{
+    return
+        pos == chain.size()?
+            from
+        :
+            chain[pos]->wrap_expr(
+                use_conv_chain(from, chain, pos+1));
+}
+
+ExprPtr TypeConvGraph::wrap_expr(ExprPtr from_expr, BetterTypeInfo from, BetterTypeInfo to) const
+{
+    vertex_t source = vertex_map.find(from)->second;
+    vertex_t dest = vertex_map.find(to)->second;
+
+    try
+    {
+        breadth_first_search(graph, source, visitor(ConvChainBuilder(dest)));
+    }
+    catch (conv_vect_t const& chain)
+    {
+        return use_conv_chain(from_expr, chain);
+    }
+    throw std::logic_error(std::string("No type conversion path connecting ") + from.describe() + " to " + to.describe());
+
 }
 
 }}
