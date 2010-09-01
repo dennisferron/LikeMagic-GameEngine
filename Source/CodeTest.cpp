908187076fe0c97de44e72a2db93d10ad016f30b
Dennis Ferron <dennis.ferron@gmail.com>
2010-09-01 00:28:30 -0500

    Sketching out how to add fast-call optimization to make commonly used functions faster.

--- Source/CodeTest.cpp
+++ Source/CodeTest.cpp
@@ -1,7 +1,52 @@
-#include <boost/type_traits/is_enum.hpp>
 
-enum E1 { a, b, c };
-enum class E2 { a, b, c };
+#include <iostream>
 
-static_assert(boost::is_enum<E1>::value, "regular enum");
-static_assert(boost::is_enum<E2>::value, "new enums");
+#include "LikeMagic/Utility/TupleForEach.hpp"
+
+std::string a() { return "a called"; }
+std::string b() { return "b called"; }
+
+template <typename F, F f>
+struct IoCall
+{
+    static void doit() { std::cout << "IoCall " << (*f)() << std::endl; };
+};
+
+template <typename F, F f>
+struct V8Call
+{
+    void doit() { std::cout << "V8Call " << (*f)() << std::endl; };
+};
+
+
+
+template<typename F, F f, template<typename F, F f> class... Policies >
+struct Test
+{
+    struct Functor
+    {
+        template <typename Policy>
+        void operator ()(Policy p)
+        {
+            p.doit();
+        }
+    };
+
+    static void doit()
+    {
+        auto policies = std::make_tuple(Policies<F, f>()...);
+        Functor action;
+        LikeMagic::Utility::tuple_for_each(action, policies);
+    }
+};
+
+struct Runner
+{
+    Runner()
+    {
+        Test<decltype(&a), &a, IoCall, V8Call>::doit();
+        Test<decltype(&b), &b, IoCall, V8Call>::doit();
+    }
+} instance;
+
+
