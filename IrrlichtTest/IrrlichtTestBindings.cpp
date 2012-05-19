e8ae141c76c08fe4cca06308438420ddc7404354
Dennis Ferron <dennis.ferron@gmail.com>
2012-05-19 14:03:05 -0500

    Saving work.

---
+++ IrrlichtTest/IrrlichtTestBindings.cpp
@@ -0,0 +1,28 @@
+// LikeMagic C++ Binding Library
+// Copyright 2008-2011 Dennis Ferron
+// Co-founder DropEcho Studios, LLC.
+// Visit our website at dropecho.com.
+//
+// LikeMagic is BSD-licensed.
+// (See the license file in LikeMagic/Licenses.)
+
+#include "Bindings/IrrlichtTest/Bindings.hpp"
+#include "LikeMagic/Utility/UserMacros.hpp"
+
+using namespace LikeMagic;
+
+int runTest();
+
+namespace Bindings { namespace IrrlichtTest {
+
+DLL_PUBLIC void add_bindings(RuntimeTypeSystem& type_sys)
+{
+    // This needs to be done once in every DLL.
+    LM_SET_TYPE_INFO(type_sys)
+
+    auto ns_irrlicht_test = Namespace::global(type_sys).subspace("IrrlichtTest");
+
+    LM_STATIC_FUNC(ns_irrlicht_test,, runTest)
+}
+
+}}
---
+++ IrrlichtTest/main.cpp
@@ -0,0 +1,43 @@
+// This is a Demo of the Irrlicht Engine (c) 2005-2009 by N.Gebhardt.
+// This file is not documented.
+
+#include <irrlicht.h>
+#ifdef _IRR_WINDOWS_
+#include <windows.h>
+#endif
+
+#include <stdio.h>
+
+#include "CMainMenu.h"
+#include "CDemo.h"
+
+using namespace irr;
+
+int runTest()
+{
+	bool fullscreen = false;
+	bool music = true;
+	bool shadows = false;
+	bool additive = false;
+	bool vsync = false;
+	bool aa = false;
+
+//#ifndef _IRR_WINDOWS_
+	video::E_DRIVER_TYPE driverType = video::EDT_OPENGL;
+//#else
+//	video::E_DRIVER_TYPE driverType = video::EDT_DIRECT3D9;
+//#endif
+
+	CMainMenu menu;
+
+//#ifndef _DEBUG
+	if (menu.run(fullscreen, music, shadows, additive, vsync, aa, driverType))
+//#endif
+	{
+		CDemo demo(fullscreen, music, shadows, additive, vsync, aa, driverType);
+		demo.run();
+	}
+
+	return 0;
+}
+
