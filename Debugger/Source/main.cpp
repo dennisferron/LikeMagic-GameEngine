be94c5938777fbdf8c645a503518deee5c152727
Dennis Ferron <dennis.ferron@gmail.com>
2012-06-09 16:53:38 -0500

    Got basic I/O and stream capturing with boost process working.

--- Debugger/Source/main.cpp
+++ Debugger/Source/main.cpp
@@ -9,23 +9,26 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#define BOOST_FILESYSTEM_VERSION 2
 #include <boost/process.hpp>
+
 #include <string>
 #include <vector>
 #include <iostream>
 
 namespace bp = ::boost::process;
 
+
 bp::child start_child()
 {
-    std::string exec = "ls";
+    std::string exec = "/bin/cat";
 
     std::vector<std::string> args;
-    args.push_back("ls");
-    args.push_back("~");
+    args.push_back("/bin/cat");
 
     bp::context ctx;
     ctx.stdout_behavior = bp::capture_stream();
+    ctx.stdin_behavior = bp::capture_stream();
 
     return bp::launch(exec, args, ctx);
 }
@@ -34,8 +37,14 @@ int main()
 {
     bp::child c = start_child();
 
+    bp::postream &os = c.get_stdin();
+    os << "line 1" << std::endl << "line 2" << std::endl << "line 3" << std::endl;
+    os.close();
+
     bp::pistream &is = c.get_stdout();
     std::string line;
     while (std::getline(is, line))
         std::cout << line << std::endl;
+
+    return 0;
 }
