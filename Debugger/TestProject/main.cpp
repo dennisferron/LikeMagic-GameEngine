dd932fff5661c8b28bdb12efce38dfc9ee19e4be
Dennis Ferron <dennis.ferron@gmail.com>
2012-08-05 18:59:12 -0500

    Added breakpoint functions to testproject and updated log to reflect expected output when setting a breakpoint.

--- Debugger/TestProject/main.cpp
+++ Debugger/TestProject/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+struct Breakpoint
+{
+    std::string file_name;
+    int line_number;
+};
+
+std::vector<Breakpoint> breakpoints;
+
+extern "C"
+{
+#pragma GCC push_options
+#pragma GCC optimize ("0")
+
+void io_debugger_break_here(int breakpoint_number, char const* file_name, int line_number)
+{
+    // Prevent this function from being optimized away.
+    asm ("nop;");
+}
+
+int io_debugger_set_breakpoint(char const* file_name, int line_number)
+{
+    breakpoints.push_back({file_name, line_number});
+    return breakpoints.size();
+}
+
+#pragma GCC pop_options
+}
+
 
 int main()
 {
     cout << "Hello world!" << endl;
+
+    // Simulate hitting a breakpoint.
+    if (!breakpoints.empty())
+        io_debugger_break_here(1, breakpoints.at(0).file_name.c_str(), breakpoints.at(0).line_number);
+
+    cout << "Goodbye, cruel world!" << endl;
     return 0;
 }
