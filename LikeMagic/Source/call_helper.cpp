23d8ffb7a6c3d48308caa6f128d86162acbab36e
Dennis Ferron <dennis.ferron@gmail.com>
2014-11-14 18:18:40 -0600

    Fix build errors

--- LikeMagic/Source/call_helper.cpp
+++ LikeMagic/Source/call_helper.cpp
@@ -1,10 +1,26 @@
 #include "LikeMagic/Exprs/call_helper.hpp"
+#include "LikeMagic/Mirrors/TypeMirror.hpp"
+#include "LikeMagic/Mirrors/CallTarget.hpp"
+#include "LikeMagic/TypeSystem.hpp"
 
 namespace LM {
 
 ExprPtr call(ExprPtr expr, std::string name, ExprPtr args[], int size)
 {
-    throw std::logic_error("not implemented");
+    TypeMirror* type_mirror = type_system->get_class(expr->get_type());
+    auto* method = type_mirror->get_method(name, size);
+
+    if (!method)
+    {
+        // Don't pass on to IoObject_perform if the problem is just wrong number of args;
+        // use suggest method to give a good error message instead.
+        if (type_mirror->has_method_named(name))
+            type_mirror->suggest_method(name, size);
+        else
+            throw std::logic_error("Method " + name + " not found in type " + type_mirror->description());
+    }
+
+    return method->call(expr, args);
 }
 
 }
